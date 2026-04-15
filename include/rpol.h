f4307391572dc8869b3e152578124639bbd6246e
sherifHaraedy <sherifHaraedy@github.com>
2026-04-15 07:09:20 -0500

    added compile-time check + re-wrote rpol itself

--- include/rpol.h
+++ include/rpol.h
@@ -1,65 +1,128 @@
 #ifndef __RPOL__
 #define __RPOL__
 
+#include <cstddef>
+#include <cstdbool>
 #include <string>
 #include <algorithm>
-#include <utility>
+#include <concepts>
 
-const std::string_view L("*+?ab");
-
+namespace {
 std::string_view torpol(std::string in)
 {
-	for (auto it = in.begin(); it != in.end(); ++it) {
-		if (*it == '|') {
-			if (std::next(it, 2) != in.end()) {
-				char tmp = *it;
-				std::for_each(L.cbegin(), L.cend(), [&it,tmp](char R) {
-					if (*(it+2) == R) {
-						*it = *(it+1);
-						*(it+1) = *(it+2);
-						*(it+2) = tmp;
+	const std::string_view L("*+?");
 
-						it += 1;
-					}
-				});
-			} else {
+	int Rc = 0;
+	for (auto rit = in.rbegin(); rit < in.rend()-1; ++rit) {
+		std::for_each(L.cbegin(), L.cend(), [rit,&Rc](const char R) {
+			if (*rit == R) Rc = 2;
+		});
 
-				std::swap(*it, *(it+1));	
-			}
+		if (*rit != '|') Rc++;
+		
+		if (*rit == '|' && Rc > 1) {
+			char tmp = *rit;
 
-			
-			it += 1;
-		} 
+			*rit = *(rit-1);
+			*(rit-1) = *(rit-2);
+			*(rit-2) = tmp;
+
+			Rc = 0;
+		} else if (*rit == '|') {
+			std::swap(*rit, *(rit-1));
+		}
 	}
 
 	return std::string_view(in);
 };
 
+template <std::size_t N>
+struct Literal
+{
+	static constexpr std::size_t size = N;
+	char data[N] { };
+
+	consteval Literal(const char (&s)[N])
+	{
+		std::copy(s, s + N, &data[0]);
+	}
+};
+
+template <Literal S>
+concept is_regex_v = requires {
+	requires []() constexpr {
+		const std::string_view R("*+?|");
+
+		for (const char s : S.data) {
+			if (s == '\0') break;
+
+			if (s > 122) {
+				bool tmp = false;
+				for (const char r : R) {
+					if (s == r) tmp = true;
+				}
+
+				if (!tmp) return false;
+			}
+		}
+
+		return true;
+	}();
+};
+
+template <Literal S>
+struct Is_Regex {
+	static constexpr auto value = S;
+};
+
 struct Regex {
-	std::string_view expr { };
+	std::string _m { };
+ 	std::string_view expr { };
 	std::string_view::const_iterator it { };
 
-	explicit Regex(std::string fexpr) : expr(torpol(std::move(fexpr)))
-	{	
-		this->expr = fexpr;
-		this->it   = this->expr.cbegin();
-	}
+	Regex() = default;
 
-	template<typename T>
-	Regex(T) = delete;
+	template <Literal S>
+	Regex(Is_Regex<S>&&) : _m(torpol(std::string(S.data)))
+	{
+		expr = _m;
+		it   = expr.cbegin();
+	}	
 
-	Regex() = delete;
 	Regex& operator=(Regex& fregex) = delete;
 
-	Regex& operator=(const Regex& fregex) noexcept
+	Regex(Regex&& fregex) noexcept : _m(std::move(fregex._m))
+	{
+		this->expr = std::string_view(this->_m);
+		this->it   = this->expr.cbegin() + std::distance(fregex.expr.cbegin(), fregex.it);
+
+		fregex.expr = { };
+	}
+
+	Regex& operator=(Regex&& fregex) noexcept
 	{
 		if (this != &fregex) {
-			this->expr = fregex.expr;
+			this->_m   = std::move(fregex._m);
+			this->expr = std::string_view(this->_m);
 			this->it   = this->expr.cbegin() + std::distance(fregex.expr.cbegin(), fregex.it);
+
+			fregex.expr = { };
 		}
 
 		return *this;
 	}
+
+	#ifdef __DEBUG_BUILD
+	#include <iostream>
+	void print() const { std::cout << expr << '\n'; }
+	#endif
 };
 
+template <Literal S> requires is_regex_v<S>
+constexpr auto operator"" _re()
+{
+	return ::Is_Regex<S>{ };
+}
+
+}
 #endif /* __RPOL__ */
