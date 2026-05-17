eb8674e429205137c8db0a586291c175901b9911
sherifHaraedy <sherifHaraedy@github.com>
2026-05-17 02:44:11 -0500

    added state class + nfa class definitions

--- include/ratomic.h
+++ include/ratomic.h
@@ -1,81 +1,51 @@
 #ifndef __RATOMIC__
 #define __RATOMIC__
 
-#include <type_traits>
-#include <vector>
-
 #include "rpol.h"
+#include <vector>
+#include <type_traits>
 
 template <typename>
-struct is_std_vector : std::false_type {};
+struct is_std_vector : std::false_type { };
 
 template <typename T, typename U>
-struct is_std_vector<std::vector<T, U>> : std::true_type {};
+struct is_std_vector<std::vector<T,U>> : std::true_type { };
 
-template <typename T>
+template<typename T>
 concept is_std_vector_v = requires { is_std_vector<T>::value; };
 
 struct Q {
-    std::vector<unsigned char> q{};
+	std::vector<unsigned char> q { };
+	
+	template <typename T> requires is_std_vector_v<T>
+	Q(T fq) : q(fq) { }
 
-    template <typename T>
-        requires is_std_vector_v<T>
-    Q(T fq) : q(fq)
-    {
-    }
+	template<typename T>
+	Q(T) = delete;
 
-    template <typename T>
-    Q(T) = delete;
+	template <typename T> requires is_std_vector_v<T>
+	Q(T&& fq) noexcept : q(std::move(fq)) { }
 
-    template <typename T>
-        requires is_std_vector_v<T>
-    Q(T&& fq) noexcept : q(std::move(fq))
-    {
-    }
+	Q& operator=(const Q&) = delete;
+	Q& operator=(Q&& fQ) noexcept
+	{
+		if (this != &fQ) this->q = std::move(fQ.q);
 
-    Q& operator=(const Q&) = delete;
-    Q& operator=(Q&& fQ) noexcept
-    {
-        if (this != &fQ) this->q = std::move(fQ.q);
-
-        return *this;
-    }
+		return *this;
+	}
 };
 
-typedef Q (&Delta)(Q, Regex&&);
+using Delta = Q (*)(Q, Regex&&);
 
 struct Nfa {
-    Q Qm;
-    Q Fm;
+	Q Qm;
+	Q Fm;
 
-    const std::string_view L{};
-    const unsigned char q0{};
-    Delta Dm;
+	const std::string_view L { };
+	const unsigned char q0 { };
+	Delta Dm;
 
-    void read_tape(Regex);
-};
-
-template <std::size_t N = 2>
-struct Stack {
-    std::size_t capacity = N;
-    Regex *const base = nullptr, *top = nullptr;
-
-    Stack(Regex* const fbase) : base(fbase) { top = base; }
-
-    Stack(Stack&&);
-
-    Stack& operator=(const Stack&) = delete;
-
-    Stack& operator=(Stack&&);
-
-    void resize(std::size_t) const;
-
-    void push(Regex&& r);
-    void pop(Regex&& r);
-
-    void peek() const;
-
-    ~Stack();
+	void read_tape(Regex);
 };
 
 #endif /* __RATOMIC__ */
